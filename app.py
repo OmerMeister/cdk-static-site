17af3f39368639cede60576761d6398ae1080128
Omer Meister <stifmayster@yahoo.com>
2023-09-26 17:48:06 +0300

    initial commit, just to get all files in one place

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python3
 
-import aws_cdk as cdk
+import os
+from aws_cdk import App, Environment
+from site_stack import StaticSiteStack
 
-from cdk_static_site.cdk_static_site_stack import CdkStaticSiteStack
 
+app = App()
+props = {
+    "namespace": app.node.try_get_context("namespace"),
+    "domain_name": app.node.try_get_context("domain_name"),
+    "sub_domain_name": app.node.try_get_context("sub_domain_name"),
+    "domain_certificate_arn": app.node.try_get_context(
+        "domain_certificate_arn"
+    ),
+    "enable_s3_website_endpoint": app.node.try_get_context(
+        "enable_s3_website_endpoint"
+    ),
+    "origin_custom_header_parameter_name": app.node.try_get_context(
+        "origin_custom_header_parameter_name"
+    ),
+    "hosted_zone_id": app.node.try_get_context("hosted_zone_id"),
+    "hosted_zone_name": app.node.try_get_context("hosted_zone_name"),
+}
 
-app = cdk.App()
-CdkStaticSiteStack(app, "CdkStaticSiteStack")
+env = Environment(
+    account=os.environ.get(
+        "CDK_DEPLOY_ACCOUNT", os.environ.get("CDK_DEFAULT_ACCOUNT")
+    ),
+    region=os.environ.get(
+        "CDK_DEPLOY_REGION", os.environ.get("CDK_DEFAULT_REGION")
+    ),
+)
+
+StaticSite = StaticSiteStack(
+    scope=app,
+    construct_id=f"{props['namespace']}-stack",
+    props=props,
+    env=env,
+    description="Static Site using S3, CloudFront and Route53",
+)
 
 app.synth()
--- cdk_static_site/cdk_static_site_stack.py
+++ cdk_static_site/cdk_static_site_stack.py
@@ -1,26 +1,55 @@
-from constructs import Construct
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
+from aws_cdk import CfnOutput, Stack
+from static_site import StaticSitePublicS3, StaticSitePrivateS3
 
 
-class CdkStaticSiteStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+class StaticSiteStack(Stack):
+    def __init__(self, scope, construct_id, props, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkStaticSiteQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
+        site_domain_name = props["domain_name"]
+        if props["sub_domain_name"]:
+            site_domain_name = (
+                f'{props["sub_domain_name"]}.{props["domain_name"]}'
+            )
 
-        topic = sns.Topic(
-            self, "CdkStaticSiteTopic"
-        )
+        # If S3 website endpoint enabled, it creates the static site using a
+        # public S3 as the origin. Otherwise, it creates a private S3 as the
+        # origin.
+        if props["enable_s3_website_endpoint"]:
+            site = StaticSitePublicS3(
+                self,
+                f"{props['namespace']}-construct",
+                site_domain_name=site_domain_name,
+                domain_certificate_arn=props["domain_certificate_arn"],
+                origin_referer_header_parameter_name=props[
+                    "origin_custom_header_parameter_name"
+                ],
+                hosted_zone_id=props["hosted_zone_id"],
+                hosted_zone_name=props["hosted_zone_name"],
+            )
+        else:
+            site = StaticSitePrivateS3(
+                self,
+                f"{props['namespace']}-construct",
+                site_domain_name=site_domain_name,
+                domain_certificate_arn=props["domain_certificate_arn"],
+                hosted_zone_id=props["hosted_zone_id"],
+                hosted_zone_name=props["hosted_zone_name"],
+            )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Add stack outputs
+        CfnOutput(
+            self,
+            "SiteBucketName",
+            value=site.bucket.bucket_name,
+        )
+        CfnOutput(
+            self,
+            "DistributionId",
+            value=site.distribution.distribution_id,
+        )
+        CfnOutput(
+            self,
+            "CertificateArn",
+            value=site.certificate.certificate_arn,
+        )
